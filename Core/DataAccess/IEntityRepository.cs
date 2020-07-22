64e80024a0eecb93534c647e581cb6dd8ab8b53b
Gkhan2394 <gokhan2394@gmail.com>
2020-07-22 22:59:33 +0300

    IEntityRepository interface added

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class
+    {
+        T Get(Expression<Func<T, bool>> filter);
+
+        IList<T> GetList(Expression<Func<T, bool>> filter=null);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
